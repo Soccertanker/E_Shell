1c31f8bc647e95a1376ede124af72059f60b2e8a
Soccertanker <e.smith.py@gmail.com>
2019-09-17 11:57:29 -0700

    working on get_job and get_process

--- shell.c
+++ shell.c
@@ -46,12 +46,14 @@ struct arg {
  * process - a single process that can be executed
  *
  * argv: argument strings of process
+ * numArgs: number of arguments in argv list
  * completed: true if process has completed
  * status: exit status of process
  * nextProcess: pointer to process that output will be redirected to
  */
 struct process {
 	char **argv;
+	size_t numArgs;
 
 	short completed;
 	int status;
@@ -66,6 +68,7 @@ struct process {
  * inputRedirectFile: name of file for input redirect
  * outputRedirectFile: name of file for output redirect
  * firstProcess: first process in list of processes
+ * numProcesses: number of processes in process list
  * nextJob: next job in list of background jobs
  */
 struct job {
@@ -75,6 +78,7 @@ struct job {
 	char *outputRedirectFile;
 
 	process_t firstProcess;
+	size_t numProcesses;
 
 	job_t nextJob;
 };
@@ -85,7 +89,24 @@ job_t firstBackgroundJob;
 
 arg_t arg;
 
+/**
+ * null_ptr_check - checks if pointer is null
+ *
+ * ptr: pointer to check
+ *
+ * Returns -1 if pointer is null.
+ */
+#define null_ptr_check(ptr)		\
+	do {				\
+		if (!ptr)		\
+			return -1;	\
+	} while (0)
 
+/**
+ * cond_free - frees object if object is not null
+ *
+ * obj: object to free
+ */
 #define cond_free(obj)			\
 	do {				\
 		if (obj)		\
@@ -94,16 +115,26 @@ arg_t arg;
 
 #define prompt_user() printf("\nE_shell $ ")
 
-void destroy_job(job_t job)
+int destroy_job(job_t job)
 {
+	null_ptr_check(job);
 	cond_free(job->line);
 	cond_free(job->inputRedirectFile);
 	cond_free(job->outputRedirectFile);
-	free(job);
+	cond_free(job);
 }
 
 /**
- * get_process - gets a process from part of a command line
+ * get_arg - reads the next argument from a process
+ *
+ */
+int get_arg(process_t process, char* line)
+{
+	return 0;
+}
+
+/**
+ * get_process - reads the next process for a job
  *
  * This function parses a line for arguments until a process's line is finished.
  * This finish is marked by a pipe, which indicates that a new process's
@@ -113,17 +144,29 @@ void destroy_job(job_t job)
  * parentJob: job where the process will be added to
  * line: pointer to character where the process's arguments begin
  *
- * Returns -1 if parsing error occurs, number of characters read from line
- * otherwise.
+ * Returns -1 if parsing error occurs.
+ * Returns 0 if there are no processes to be parsed.
+ * Returns 1 if a process was successfully parsed.
  */
 int get_process(job_t parentJob, char *line)
 {
-	do {
-		get_arg(line);
-	} while (0);
+	process_t process = (process_t) calloc(1, sizeof(struct process));
+	if (!process)
+		return NULL;
+
+
+
+	return 0;
 }
 
-/* Convert a command line to a job. */
+/*
+ * Convert a command line to a job.
+ *
+ * line: pointer to character where user's line begins
+ *
+ * Returns NULL if malloc fails or if there is a parsing error.
+ * Returns job otherwise.
+ */
 job_t get_job(char *line)
 {
 	job_t job = (job_t) calloc(1, sizeof(struct job));
@@ -131,12 +174,18 @@ job_t get_job(char *line)
 		return NULL;
 
 	char *lineRemaining = line;
-	size_t numCharsRead;
+	size_t numProcessesParsed;
 
-	/* Get all processes from a command line. */
-	while (numCharsRead = get_process(job, &lineRemaining) > 0)
+	/* Get all processes from a job. */
+	while (numProcessesParsed = get_process(job, lineRemaining) > 0);
 
-	return 0;
+	/* get_process encountered a parsing error. */
+	if (numProcessesParsed == -1) {
+		destroy_job(job);
+		return NULL;
+	}
+
+	return job;
 }
 
 int start_job(job_t job)
